730c7cfd0253f41273208cb3e530fee85199f9f1
kdaou <khalil.daou@gmail.com>
2018-04-18 09:25:56 +0200

    project initialisation

---
+++ src/main/java/com/alex/services/matching/taxi/AlexServicesApplication.java
@@ -0,0 +1,12 @@
+package com.alex.services.matching.taxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexServicesApplication.class, args);
+	}
+}
